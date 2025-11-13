7c1ddbdb0864660c06b416921ac9ed562c76957b
hendra.sasmita <hendra.sasmita>
2025-11-13 12:41:30 +0800

    main: try submissions proto

---
+++ compile.sh
@@ -0,0 +1,3 @@
+protoc -I=/opt/as/APP/proto/jform-protos/protobuf \ 
+    --go_out=/opt/as/APP/proto/jform-protos/go \
+    /opt/as/APP/proto/jform-protos/protobuf/submissions/submission.proto
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jebing/jform-protos
+
+go 1.24.1
